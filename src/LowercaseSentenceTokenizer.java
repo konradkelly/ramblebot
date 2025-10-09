d1f90f0c0ab8e5b1e58f8587f99944da4558993d
konradkelly <konradky@gmail.com>
2025-10-08 20:59:30 -0700

    implemented Wave One

--- src/LowercaseSentenceTokenizer.java
+++ src/LowercaseSentenceTokenizer.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * A tokenizer that converts text input to lowercase and splits it 
@@ -28,9 +29,15 @@ public class LowercaseSentenceTokenizer implements Tokenizer {
    * @param scanner the Scanner to read the input text from
    * @return a list of tokens, where each token is a word or a period
    */
-  public List<String> tokenize(Scanner scanner) {
-    // TODO: Implement this function to convert the scanner's input to a list of words and periods
-    return null;
+  public List<String> tokenize(Scanner scanner) {    
+    List<String> tokens = new ArrayList<>();
+
+    while (scanner.hasNext()) {
+      tokens.add(scanner.next());   
+    }
+    
+
+    return tokens;
   }
 }
 
